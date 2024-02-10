1f3c4e889d9dac1022ab9bb6da430bd6d6add829
Shayxon <shayxon1@gmail.com>
2024-02-10 20:17:29 +0500

    html and css for posts

--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Post
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html')
+    posts = Post.objects.filter(author = request.user.id)
+    return render(request, 'account/dashboard.html', {'posts':posts})
 
 @login_required
 def edit(request):
