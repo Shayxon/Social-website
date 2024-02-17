9c855c4c452751574631e0b57346412cfc7dab7b
Shayxon <shayxon1@gmail.com>
2024-02-17 19:54:14 +0500

    switch database to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_website',
+        'USER': 'postgres',
+        'PASSWORD': 'Toirjonov2006',
     }
 }
 
